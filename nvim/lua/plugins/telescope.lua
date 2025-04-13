da3d94aa3b25b41f7d29daa93781e4753ddf9578
Y.S <39@no.email>
2025-04-13 09:47:01 +0800

    add dependencies: nvim-web-devicons

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ local M = {
 
 M.dependencies = {
         "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
         "nvim-telescope/telescope-ui-select.nvim",
         {
                 "nvim-telescope/telescope-fzf-native.nvim",
