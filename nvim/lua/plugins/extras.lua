b740e874d80343f16d4d2de9b08385caeb2da0a2
Y.S <39@no.email>
2025-04-15 16:25:36 +0800

    rename:sundry.lua to extras.lua

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -4,55 +4,55 @@ M[#M + 1] = {
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {
                 winopts = {
-                        height   = 0.40,
-                        width    = 1,
-                        row      = 1,
-                        col      = 0,
+                        height = 0.40,
+                        width = 1,
+                        row = 1,
+                        col = 0,
                         backdrop = 100,
-                        border   = { '─', '─', '─', ' ', ' ', ' ', ' ', ' ' },
-                        preview  = {
-                                border     = { '─', '─', '─', ' ', ' ', ' ', ' ', ' ' },
-                                horizontal = 'right:50%',
-                                title      = false,
-                                scrollbar  = false,
-                                winopts    = {
+                        border = { "─", "─", "─", " ", " ", " ", " ", " " },
+                        preview = {
+                                border = { "─", "─", "─", " ", " ", " ", " ", " " },
+                                horizontal = "right:50%",
+                                title = false,
+                                scrollbar = false,
+                                winopts = {
                                         number = false,
                                 },
                         },
                 },
-                files   = {
-                        previewer = false
-                }
+                files = {
+                        previewer = false,
+                },
         },
         event = "VeryLazy",
 }
 
 --PLUG: oil
 M[#M + 1] = {
-        'stevearc/oil.nvim',
+        "stevearc/oil.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {
+        config = {
                 default_file_explorer = false,
                 delete_to_trash = true,
                 skip_confirm_for_simple_edits = true,
                 watch_for_changes = true,
                 use_default_keymaps = false,
                 keymaps = {
-                        ["g?"]    = { "actions.show_help", mode = "n" },
+                        ["g?"] = { "actions.show_help", mode = "n" },
                         ["<C-R>"] = "actions.refresh",
-                        ["<CR>"]  = "actions.select",
+                        ["<CR>"] = "actions.select",
                         ["<C-s>"] = { "actions.select", opts = { vertical = true } },
                         ["<C-v>"] = { "actions.select", opts = { horizontal = true } },
                         ["<C-t>"] = { "actions.select", opts = { tab = true } },
-                        ["q"]     = { "actions.close", mode = "n" },
-                        ["H"]     = { "actions.parent", mode = "n" },
-                        ["L"]     = "actions.select",
-                        ["="]     = "actions.open_external",
-                        ["gs"]    = { "actions.change_sort", mode = "n" },
-                        ["."]     = { "actions.toggle_hidden", mode = "n" },
-                        [","]     = { "actions.open_cwd", mode = "n" },
-                        ["`"]     = { "actions.cd", mode = "n" },
-                        ["~"]     = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+                        ["q"] = { "actions.close", mode = "n" },
+                        ["H"] = { "actions.parent", mode = "n" },
+                        ["L"] = "actions.select",
+                        ["="] = "actions.open_external",
+                        ["gs"] = { "actions.change_sort", mode = "n" },
+                        ["."] = { "actions.toggle_hidden", mode = "n" },
+                        [","] = { "actions.open_cwd", mode = "n" },
+                        ["`"] = { "actions.cd", mode = "n" },
+                        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
                 },
                 view_options = {
                         show_hidden = true,
@@ -74,15 +74,13 @@ M[#M + 1] = {
                                         row = 0,
                                         col = ui.width - sidebar_width,
                                         style = "minimal",
-                                        border = "single"
+                                        border = "single",
                                 }
 
                                 return conf
-                        end
-                }
+                        end,
+                },
         },
         event = "VeryLazy",
-
 }
-
 return M
