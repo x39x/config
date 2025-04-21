abe8575057ef44d47454542f1e2c824a5005481c
Matsu <matsu3x9@icloud.com>
2025-04-22 02:23:50 +0800

    add new lsp config

--- nvim/lsp/pyright.lua
+++ nvim/lsp/pyright.lua
@@ -1,16 +1,46 @@
--- https://github.com/neovim/nvim-lspconfig/issues/500#issuecomment-876700701
+local lsp_keymaps = require("utils.lsp_keymaps")
 
-local python_path
-if vim.env.VIRTUAL_ENV then
-        python_path = vim.env.VIRTUAL_ENV .. "/" .. "bin" .. "/" .. "python"
-else
-        python_path = vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
+local function organize_imports()
+        local params = {
+                command = "pyright.organizeimports",
+                arguments = { vim.uri_from_bufnr(0) },
+        }
+
+        local clients = vim.lsp.get_clients({
+                bufnr = vim.api.nvim_get_current_buf(),
+                name = "pyright",
+        })
+        for _, client in ipairs(clients) do
+                client:request("workspace/executeCommand", params, nil, 0)
+        end
+end
+
+--NOTE: https://github.com/neovim/nvim-lspconfig/issues/500#issuecomment-876700701
+local function get_python_path()
+        local python_path
+        if vim.env.VIRTUAL_ENV then
+                python_path = vim.env.VIRTUAL_ENV .. "/" .. "bin" .. "/" .. "python"
+        else
+                python_path = vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
+        end
+        return python_path
 end
 
 return {
         on_init = function(client)
-                client.config.settings.python.pythonPath = python_path
+                client.config.settings.python.pythonPath = get_python_path()
         end,
+        cmd = { "pyright-langserver", "--stdio" },
+        filetypes = { "python" },
+        root_markers = {
+                "pyproject.toml",
+                "setup.py",
+                "setup.cfg",
+                "requirements.txt",
+                "Pipfile",
+                "pyrightconfig.json",
+                ".git",
+        },
         settings = {
                 python = {
                         analysis = {
@@ -18,4 +48,10 @@ return {
                         },
                 },
         },
+        on_attach = function(_, bufnr)
+                vim.api.nvim_buf_create_user_command(0, "PyrightOrganizeImports", organize_imports, {
+                        desc = "Organize Imports",
+                })
+                lsp_keymaps(bufnr)
+        end,
 }
