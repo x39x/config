0d91385281e85f62b59089f906b866279bc2929b
Matsu <matsu3x9@icloud.com>
2025-04-26 16:52:24 +0800

    spell

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -45,9 +45,11 @@ M[#M + 1] = {
         config = function()
                 local null_ls = require("null-ls")
                 local formatting = null_ls.builtins.formatting
+                local diagnostics = null_ls.builtins.diagnostics
                 null_ls.setup({
                         debug = false,
                         sources = {
+                                diagnostics.codespell,
                                 -- To increase speed, try prettierd.
                                 formatting.prettier.with({
                                         extra_args = { "--tab-width", "4" },
