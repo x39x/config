2f3df8da3792c6324927b11fef2efad40757efa8
Matsu <matsu3x9@icloud.com>
2025-04-30 00:38:57 +0800

    Wed Apr 30 00:38:57 CST 2025

--- nvim/lua/utils/gtags.lua
+++ nvim/lua/utils/gtags.lua
@@ -1,42 +1,51 @@
 local M
 M.goto_definition = function()
         local word = vim.fn.expand("<cword>")
-        local cmd = "global -ax " .. word
-        local output = vim.fn.systemlist(cmd)
 
-        if vim.v.shell_error ~= 0 or #output == 0 then
-                vim.notify("Definition not found for: " .. word, vim.log.levels.WARN)
-                return
-        end
+        vim.system({ "global", "-ax", word }, { text = true }, function(result)
+                if result.code ~= 0 or not result.stdout then
+                        vim.notify("Definition not found for: " .. word, vim.log.levels.WARN)
+                        return
+                end
 
-        local result = output[1]
-        local parts = {}
-        for v in result:gmatch("%S+") do
-                table.insert(parts, v)
-        end
+                local output = vim.split(result.stdout, "\n", { trimempty = true })
 
-        local file = parts[3]
-        local line = parts[2]
-        vim.notify(file .. "" .. line, vim.log.levels.INFO)
+                if #output == 0 then
+                        vim.notify("Definition not found for: " .. word, vim.log.levels.WARN)
+                        return
+                end
 
-        if file and line then
-                vim.cmd("edit " .. file)
-                vim.cmd(":" .. line)
-        else
-                vim.notify("Invalid result: " .. result, vim.log.levels.WARN)
-        end
+                local first_result = output[1]
+                local parts = vim.split(first_result, "%s+")
+
+                local file = parts[3]
+                local line = parts[2]
+
+                if file and line then
+                        vim.notify("Opening: " .. file .. " Line: " .. line, vim.log.levels.INFO)
+                        vim.cmd("edit " .. vim.fn.fnameescape(file))
+                        vim.cmd(":" .. line)
+                else
+                        vim.notify("Invalid result: " .. first_result, vim.log.levels.WARN)
+                end
+        end)
 end
 
 M.goto_references = function()
-        local word = vim.fn.expand("<cword>") -- 获取光标下的单词
-        local cmd = "global -axr " .. word
-        local output = vim.fn.systemlist(cmd)
+        local word = vim.fn.expand("<cword>")
+        vim.system({ "global", "-axr", word }, { text = true }, function(result)
+                if result.code ~= 0 or not result.stdout then
+                        vim.notify("Error! gtags.lua:39" .. word, vim.log.levels.WARN)
+                        return
+                end
 
-        if vim.v.shell_error ~= 0 or #output == 0 then
-                vim.notify("References not found for: " .. word, vim.log.levels.WARN)
-                return
-        end
+                local output = vim.split(result.stdout, "\n", { trimempty = true })
 
+                if #output == 0 then
+                        vim.notify("References not found for: " .. word, vim.log.levels.WARN)
+                        return
+                end
+        end)
         --TODO:
-        -- send to telescope/quickfix
+        -- send to telescope
 end
